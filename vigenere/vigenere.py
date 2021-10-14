c1301af98589150440578e875a280649aeb45fe9
j4wh3r <90579213+j4wh3r@users.noreply.github.com>
2021-10-14 13:10:49 +0200

    Add files via upload

---
+++ vigenere/vigenere.py
@@ -0,0 +1,29 @@
+lettres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def alphaDec_lettre(lettres,lettre):
+    index_lettre = lettres.find(lettre)
+    new_lettres = lettres[index_lettre:] +  lettres[:index_lettre]
+    return new_lettres
+def litsalpha(lettre,key):
+    l = []
+    for c in key:
+        l.append(alphaDec_lettre(lettres,c))
+    return l
+
+msg = input("enter a message: ")
+key = input("enter a key: ")
+
+def cryptmsg(msg,key):
+    encrypted = ""
+    k = 0
+    l = litsalpha(lettres,key)
+    for c in  msg:
+        pos = lettres.index(c)
+        crypted_char = l[k][pos]
+        encrypted += crypted_char
+        k = (k+1) % len(l)
+    return encrypted
+
+print(alphaDec_lettre(lettres,"V"))
+print(litsalpha(lettres,"VIGENERE"))
+print(cryptmsg(msg,key))

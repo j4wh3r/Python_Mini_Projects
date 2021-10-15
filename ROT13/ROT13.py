10416d9636adbbd3eeef9a0b1f6e94e9b5a1477c
j4wh3r <90579213+j4wh3r@users.noreply.github.com>
2021-10-15 15:43:15 +0100

    Update ROT13.py

--- ROT13/ROT13.py
+++ ROT13/ROT13.py
@@ -1,38 +1,53 @@
-message = input("enter a message: ")
+import sys,os
+
+# generating a string that contains all chars:
+alpha = ''.join(chr(i) for i in range(65,91))
+
+message = input("enter a message to decrypt: ").upper()
+
+def encryptROT(msg):
+    cryped = ''.join(alpha[(alpha.find(i)+13) % 26 ] for i in msg)
+    return cryped
+
+# by encrypting the result of the encryptROT function, the result will be the original text
+
+def decryptROT(crypt):
+    return encryptROT(crypt)
 
 
-alpha = "abcdefghijklmnopqrstuvwxyz"
+enc = encryptROT(message)
+dec = decryptROT(enc)
 
-def codageROT13(m):
-    secret = ""
-    l = []
-    for i in m:
+print(f"encrypted message= {enc}")
+print(f"decrrypted message is {dec}")
+
+##############################################################################
+
+# another version of the two functions:
+def encrypt(message):
+    encrypted = []
+    for i in message:
         if i.isalpha():
-            sec = (alpha.find(i) +13) % 26
-            l.append(alpha[sec])
+            index_of_the_char = alpha.find(i)
+            new_index = (index_of_the_char + 13) % 26
+            new_value= alpha[new_index]
+            encrypted.append(new_value)
         else:
-            l.append(i)
-    encoded = ''.join(l)
-    return encoded
-
-# def decodageROT(encoded):
-#     l = []
-#     for i in encoded:
-#         if i.isalpha():
-#             dec = (alpha.find(i) - 13) % 26
-#             l.append(alpha[dec])
-#         else:
-#             l.append(i)
-#     decoded = ''.join(l)
-#     return decoded
+            encrypted.append(i)
+    encrypted = ''.join(encrypted)
+    return encrypted
 
 
-def decodageROT(encoded):
-    return codageROT13(encoded)
-
-encoded = codageROT13(message)
-decoded = decodageROT(encoded)
-
-print(encoded)
-print(decoded)
+def decrypt(message):
+    decrypted = []
+    for i in message:
+        if i.isalpha():
+            index_of_the_char = alpha.find(i)
+            new_index = (index_of_the_char - 13) % 26
+            new_value= alpha[new_index]
+            decrypted.append(new_value)
+        else:
+            decrypted.append(i)
+    decrypted = ''.join(decrypted)
+    return decrypted
 
